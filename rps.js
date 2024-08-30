de1e161c016c85f76fd99847383d8fc134332578
WillJohnny17 <willj1705@gmail.com>
2024-08-30 13:28:10 +0100

    added the getComputerChoice function which picks one of the three choices for the computer

--- rps.js
+++ rps.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+
+
+
+
+function getComputerChoice() {
+    let choice = ["Rock", "Paper", "Scissors"];
+    let randNum = Math.floor(Math.random() * 3) + 1;
+    if (randNum === 1) {
+         choice = "Rock"
+    } else if (randNum === 2) {
+         choice = "Paper"
+    } else {
+         choice = "Scissors"
+    }
+    return choice;
+}
+
+console.log(getComputerChoice())
+
+
+
+
+
+
